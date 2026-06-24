5da2aca6787bd73b221022cbe9510f6b2e2fca92
M.Tahir Nawaz <khokhartahir290@gmail.com>
2026-06-24 14:45:55 +0500

    Set up Django admin site configuration

---
+++ cricket/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.site_header = "Universal Sports Scoreboard System Admin"
+admin.site.site_title = "USSS Project"
+admin.site.index_title = "Welcome to Sports Scoreboard System"
